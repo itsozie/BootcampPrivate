3942f505d53a2b7c89a0146709a60bb8080cc158
Ozie <itsozie77@gmail.com>
2021-08-05 01:04:04 +0700

    tambah nav admin dan user, error 404

---
+++ View/main/error.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="./assets/css/app.css">
+    <title>IvenApp</title>
+</head>
+<body>
+<div class="container">
+    <div class="error-wrapper">
+        <div class="man-icon"></div>
+        <h3 class="title">404</h3>
+        <p class="title">Server Sedang Sibuk !</p>
+    </div>
+</div>
+
+</body>
+<script src="./assets/js/bootstrap.min.js"></script>
+<script src="./assets/js/jquery-3.5.1.min.js"></script>
+<script src="./assets/js/bootstrap.js"></script>
+</html>
---
+++ View/main/navAdmin.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../assets/css/app.css">
+    <title>IvenApp</title>
+</head>
+<body>
+<nav class="navbar navbar-dark bg-dark">
+  <a class="navbar-brand">IvenApp</a>
+  <form class="form-inline">
+  <div class="dropdown">
+  <button class="btn btn-outline-success dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+    Selamat datang Admin
+  </button>
+  <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+    <a class="dropdown-item" href="#">Profil</a>
+    <a class="dropdown-item" href="#">Logout</a>
+  </div>
+</div>
+  </form>
+</nav>
+</body>
+<script src="./assets/js/bootstrap.min.js"></script>
+<script src="./assets/js/jquery-3.5.1.min.js"></script>
+<script src="./assets/js/bootstrap.js"></script>
+</html>
