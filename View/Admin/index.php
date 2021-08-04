52a908e9b35d7ff34273197113f82748dd5ee0ae
Ozie <itsozie77@gmail.com>
2021-08-05 01:15:10 +0700

    tambah card title

---
+++ View/Admin/index.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="./assets/css/app.css">
+    <title>IvenApp</title>
+</head>
+<body>
+<center>
+<div class="box">
+    <div class="container">
+     	<div class="row">
+			 
+			    <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
+               
+					<div class="box-part text-center">
+                        
+                        <i class="fa fa-instagram fa-3x" aria-hidden="true"></i>
+                        
+						<div class="title">
+							<h4>Barang</h4>
+						</div>
+                        
+						<div class="text">
+							<span>Lorem ipsum dolor sit amet, id quo eruditi eloquentiam. Assum decore te sed. Elitr scripta ocurreret qui ad.</span>
+						</div>
+                        
+						 <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Go !!!!</button>
+                        
+					 </div>
+				</div>	 
+				
+				 <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
+               
+					<div class="box-part text-center">
+					    
+					    <i class="fa fa-twitter fa-3x" aria-hidden="true"></i>
+                    
+						<div class="title">
+							<h4>User</h4>
+						</div>
+                        
+						<div class="text">
+							<span>Lorem ipsum dolor sit amet, id quo eruditi eloquentiam. Assum decore te sed. Elitr scripta ocurreret qui ad.</span>
+						</div>
+                        
+						 <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Go !!!!</button>
+                        
+					 </div>
+				</div>	 
+				
+				 <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
+               
+					<div class="box-part text-center">
+                        
+                        <i class="fa fa-facebook fa-3x" aria-hidden="true"></i>
+                        
+						<div class="title">
+							<h4>Laporan</h4>
+						</div>
+                        
+						<div class="text">
+							<span>Lorem ipsum dolor sit amet, id quo eruditi eloquentiam. Assum decore te sed. Elitr scripta ocurreret qui ad.</span>
+						</div>
+                        
+						 <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Go !!!!</button>
+                        
+					 </div>
+				</div>	 
+				
+				
+		
+		</div>		
+    </div>
+</div>
+</center>
+</body>
+<script src="./assets/js/bootstrap.min.js"></script>
+<script src="./assets/js/jquery-3.5.1.min.js"></script>
+<script src="./assets/js/bootstrap.js"></script>
+</html>
